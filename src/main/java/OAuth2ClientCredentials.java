6366873e6f0b84b51ad057aa0d12d4f73594ea93
aerkusa <jacobs85@gmail.com>
2022-10-09 13:03:01 -0400

    added errorIfNotSpecified function and loopback address

--- src/main/java/OAuth2ClientCredentials.java
+++ src/main/java/OAuth2ClientCredentials.java
@@ -1,9 +1,18 @@
+import java.sql.SQLOutput;
+
 public class OAuth2ClientCredentials {
-    public static final String DOMAIN = "http";
-    public static final String API_KEY = "123";
-    public static final String API_SECRET = "123";
+    public static final String DOMAIN = "127.0.0.1";
+    public static final String API_KEY = "Enter API Key";
+    public static final String API_SECRET = "Enter API Secret";
     public static final int PORT = 8080;
 
     public static void errorIfNotSpecified() {
+        if (API_KEY.startsWith("Enter ") || API_SECRET.startsWith("Enter ")) {
+            System.out.println(
+                    "Enter API Key and API Secret from http://www.accesslink.com/developer"
+                     + " into API_KEY and API_SECRET in "
+                     + OAuth2ClientCredentials.class);
+            System.exit(1);
+        }
     }
 }
